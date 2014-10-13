83ce348ebbca1b3474ee0e3221c7d98b89688ea1
h_sakurai <sakurai777@gmail.com>
2014-10-14 04:40:14 +0900

    added remove batfile

--- cgithubinst/tools/chocolateyInstall.ps1
+++ cgithubinst/tools/chocolateyInstall.ps1
@@ -7,6 +7,9 @@ Write-Host "Bin Root is $binRoot"
 $installDir = "$binRoot\bin\"
 if (![System.IO.Directory]::Exists($installDir)) {[System.IO.Directory]::CreateDirectory("$installDir")}
 
+Remove-Item "$installDir\cgithubinst.bat"
+Remove-Item "$installDir\ctest.bat"
+
 Get-ChocolateyWebFile "$package" "$installDir\cgithubinst.bat" "$url"
 Get-ChocolateyWebFile "$package" "$installDir\ctest.bat" "$url2"
 
