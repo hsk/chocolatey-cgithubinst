4b03d3cdea48828b1408296e00968acc7825b4ff
h_sakurai <sakurai777@gmail.com>
2014-10-14 04:34:53 +0900

    changed cgithubinst.bat

--- cgithubinst/cgithubinst.bat
+++ cgithubinst/cgithubinst.bat
@@ -3,13 +3,21 @@ if "%2" == "" goto error
 
 git clone git@github.com:%1/chocolatey-%2.git
 cd chocolatey-%2
+if "%3" == "" goto p2
+cd %3
+:p2
+cpack
+cinst -source %CD% %3 %4 %5 %6 %7
+cd ..\..
+goto end
 
+:p2
 cpack
 cinst -source %CD% %2 %3 %4 %5 %6 %7
 cd ..
 
 goto end
 :error
-@echo cgithubinst username packagename [-force]
+@echo cgithubinst username reponame [packagename] [-force]
 :end
 
